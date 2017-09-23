89beb23807157072996e3b6011708855854e54a7
Benjamin Bergstein <bennyjbergstein@gmail.com>
2017-09-22 23:34:03 -0700

    Refactor!

--- lib/stubborn.rb
+++ lib/stubborn.rb
@@ -1,5 +1,32 @@
 require "stubborn/version"
 
 module Stubborn
-  # Your code goes here...
+  def self.run_cli(argv)
+    command = argv.shift
+  
+    if command == 'new'
+      new_project(argv.shift)
+    elsif command == 'build'
+      build_project
+    elsif command == 'server'
+      run_server
+    else
+      print "No such command: '#{command}'\n"
+    end
+  end
+  
+  def self.new_project(project_directory)
+    project_template = File.expand_path('templates/project', STUBBORN_ROOT)
+    FileUtils.cp_r(project_template, project_directory)
+  end
+  
+  def self.build_project
+    locals = {}
+    result = Slim::Template.new("index.slim").render(nil, locals)
+    Pathname('site/index.html').write(result)
+  end
+  
+  def self.run_server
+    Kernel.exec('ruby -run -e httpd ./site -p 9393')
+  end
 end
